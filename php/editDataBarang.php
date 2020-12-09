7fe52c52f33fd13369c1bcf3a418b7163e65a7a9
MuhammadFahmii <fahmiprasetio754@gmail.com>
2020-12-09 10:18:37 +0700

    Merapihkan baris kode dan mengganti link asset dengan cdn

--- php/editDataBarang.php
+++ php/editDataBarang.php
@@ -1,163 +1,168 @@
-<?php 
-    session_start();
-    if(!isset($_SESSION["login"])){
-        header("location: login.php");
-        exit;
-    }
-    require "functionBarang.php";
-    $idBarang = $_GET["idBarang"];
-    $barang = query("SELECT * FROM barang WHERE idBarang = '$idBarang'")[0];
+<?php
+session_start();
+if (!isset($_SESSION["login"])) {
+  header("location: login.php");
+  exit;
+}
+require "functionBarang.php";
+$idBarang = $_GET["idBarang"];
+$barang = query("SELECT * FROM barang WHERE idBarang = '$idBarang'")[0];
 
-    if(isset($_POST["simpan"])){
-        if(edit($_POST)>0){
-            echo "
+if (isset($_POST["simpan"])) {
+  if (edit($_POST) > 0) {
+    echo "
                 <script>
                 alert('Edit data berhasil');
                 document.location.href='data-stok-barang.php';
                 </script>
             ";
-        }else{
-            echo "
+  } else {
+    echo "
                 <script>
                 alert('Edit data gagal');
                 document.location.href='data-stok-barang.php';
                 </script>
             ";
-        }
-    }
+  }
+}
 ?>
 
 <!doctype html>
 <html lang="en">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="../bootstrap-4.4.1/bootstrap-4.4.1/dist/css/bootstrap.css">
-        <link rel="stylesheet" href="../fontawesome-free-5.13.0-web/css/all.min.css">
-        <link rel="stylesheet" href="../css/style.css">     
-        
-        <title>Inventaris Gudang</title>
-    </head>
-    <body id="home">    
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <div class="wrapper">
-            <!-- Sidebar -->
-            <nav id="sidebar" class="bg-dark">
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="../index.php" class="bg-dark text-white">
-                            <i class="fas fa-tachometer-alt mr-2"></i><span class="ml-1">Beranda</span><hr class="bg-secondary mb-0">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle bg-dark text-white"><i class="fas fa-table mr-2"></i><span class="ml-1">Data Master</span><hr class="bg-secondary mb-0"></a>
-                        <ul class="collapse list-unstyled" id="homeSubmenu">
-                            <li>
-                                <a class="bg-dark text-white" href="data-stok-barang.php">
-                                    <i class="fas fa-box-open mr-2"></i><span>Data Stok Barang</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="bg-dark text-white mt-2" href="data-pelanggan.php">
-                                    <i class="fas fa-users mr-2"></i><span>Data Pelanggan</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="bg-dark text-white mt-2" href="data-pembayaran.php">
-                                    <i class="fas fa-file-invoice-dollar mr-2 ml-1"></i><span class="ml-1">Data Pembayaran</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a class="bg-dark text-white" href="barang-masuk.php">
-                            <i class="fas fa-file-invoice mr-2 ml-1"></i><span class="ml-1">Barang Masuk</span>
-                            <hr class="bg-secondary mb-0">
-                        </a>
-                    </li>
-                    <li>
-                        <a class="bg-dark text-white" href="barang-keluar.php">
-                            <i class="fas fa-dolly-flatbed mr-1"></i><span class="ml-1">Barang Keluar</span>
-                            <hr class="bg-secondary mb-0">
-                        </a>
-                    </li>                        
-                    <li>
-                        <a class="bg-dark text-white" href="pengaturan.php">
-                            <i class="fas fa-cogs mr-1"></i><span class="ml-1">Pengaturan</span>
-                            <hr class="bg-secondary mb-0">
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+  <link rel="stylesheet" href="../css/style.css">
 
-            <!-- Content -->
-            <div id="content">
-                <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-                    <a class="navbar-brand" href="#home">Aplikasi Inventori Barang</a>
-                    <button type="button" id="sidebarCollapse" class="btn btn-primary">
-                        <i class="fas fa-align-left"></i>
-                    </button>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item dropdown">            
-                                <a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="fas fa-user mr-2"></i><span><?= $_SESSION["username"]; ?></span>
-                                </a>
-                                <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
-                                    <a href="logout.php" class="dropdown-item bg-dark text-white">Logout</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-            <div class="col-12 pr-5 ml-2">
-                <h2>
-                    <i class="fas fa-file-invoice mr-2 p-4 mt-5 ml-5"></i><span>Edit Barang</span></a>
-                </h2>
-                <form action="" method="post" enctype="multipart/form-data">
-                    <input type="hidden" name="idBarang" value="<?=$barang["idBarang"];?>">
-                    <input type="hidden" name="gambarLama" id="gambarLama" value="<?= $barang["gambar"];?>">
-                    <div class="form-group">
-                        <label for="namaBarang">Nama Barang</label>
-                        <input type="text" name="namaBarang" class="form-control" id="namaBarang" value="<?= $barang["namaBarang"];?>">
-                    </div>
-                    <div class="form-group">
-                        <label for="jenisBarang">Jenis Barang</label>
-                        <input type="text" class="form-control" id="jenisBarang" name="jenisBarang" value="<?= $barang["jenisBarang"];?>" readonly>
-                        </input>
-                    </div>                    
-                    <div class="form-group">
-                        <label for="harga">Harga</label>
-                        <input type="number" name="hargaBarang" class="form-control" id="harga" value="<?= $barang["hargaJual"];?>"></input>
-                    </div>
-                    <div class="form-group">
-                        <label for="jumlah">Jumlah</label>
-                        <input type="number" name="jumlah" class="form-control" id="jumlah" value="<?= $barang["jumlah"];?>"></input>
-                    </div>
-                    <div class="form-group">
-                        <label for="gambar">Gambar</label>
-                        <img src="../img/<?= $barang["gambar"];?>" alt="" class="rounded-circle">
-                        <input type="file" name="gambar" id="gambar" class="form-control">
-                    </div>
-                    <a href="data-stok-barang.php"><button type="button" class="btn btn-danger float-right">Batal</button></a>
-                    <button type="reset" class="btn btn-warning float-right mr-2">Reset</button>
-                    <button type="submit" name="simpan" class="btn btn-primary float-right mr-2">Simpan</button>
-                  </form>      
-            </div>
+  <title>Inventaris Gudang</title>
+</head>
+
+<body id="home">
+
+  <div class="wrapper">
+    <!-- Sidebar -->
+    <nav id="sidebar" class="bg-dark">
+      <ul class="list-unstyled">
+        <li>
+          <a href="../index.php" class="bg-dark text-white">
+            <i class="fas fa-tachometer-alt mr-2"></i><span class="ml-1">Beranda</span>
+            <hr class="bg-secondary mb-0">
+          </a>
+        </li>
+        <li>
+          <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle bg-dark text-white"><i class="fas fa-table mr-2"></i><span class="ml-1">Data Master</span>
+            <hr class="bg-secondary mb-0"></a>
+          <ul class="collapse list-unstyled" id="homeSubmenu">
+            <li>
+              <a class="bg-dark text-white" href="data-stok-barang.php">
+                <i class="fas fa-box-open mr-2"></i><span>Data Stok Barang</span>
+              </a>
+            </li>
+            <li>
+              <a class="bg-dark text-white mt-2" href="data-pelanggan.php">
+                <i class="fas fa-users mr-2"></i><span>Data Pelanggan</span>
+              </a>
+            </li>
+            <li>
+              <a class="bg-dark text-white mt-2" href="data-pembayaran.php">
+                <i class="fas fa-file-invoice-dollar mr-2 ml-1"></i><span class="ml-1">Data Pembayaran</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a class="bg-dark text-white" href="barang-masuk.php">
+            <i class="fas fa-file-invoice mr-2 ml-1"></i><span class="ml-1">Barang Masuk</span>
+            <hr class="bg-secondary mb-0">
+          </a>
+        </li>
+        <li>
+          <a class="bg-dark text-white" href="barang-keluar.php">
+            <i class="fas fa-dolly-flatbed mr-1"></i><span class="ml-1">Barang Keluar</span>
+            <hr class="bg-secondary mb-0">
+          </a>
+        </li>
+        <li>
+          <a class="bg-dark text-white" href="pengaturan.php">
+            <i class="fas fa-cogs mr-1"></i><span class="ml-1">Pengaturan</span>
+            <hr class="bg-secondary mb-0">
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- Content -->
+    <div id="content">
+      <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#home">Aplikasi Inventori Barang</a>
+        <button type="button" id="sidebarCollapse" class="btn btn-primary">
+          <i class="fas fa-align-left"></i>
+        </button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user mr-2"></i><span><?= $_SESSION["username"]; ?></span>
+              </a>
+              <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
+                <a href="logout.php" class="dropdown-item bg-dark text-white">Logout</a>
+              </div>
+            </li>
+          </ul>
         </div>
+      </nav>
+    </div>
+    <div class="col-12 pr-5 ml-2">
+      <h2>
+        <i class="fas fa-file-invoice mr-2 p-4 mt-5 ml-5"></i><span>Edit Barang</span></a>
+      </h2>
+      <form action="" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="idBarang" value="<?= $barang["idBarang"]; ?>">
+        <input type="hidden" name="gambarLama" id="gambarLama" value="<?= $barang["gambar"]; ?>">
+        <div class="form-group">
+          <label for="namaBarang">Nama Barang</label>
+          <input type="text" name="namaBarang" class="form-control" id="namaBarang" value="<?= $barang["namaBarang"]; ?>">
+        </div>
+        <div class="form-group">
+          <label for="jenisBarang">Jenis Barang</label>
+          <input type="text" class="form-control" id="jenisBarang" name="jenisBarang" value="<?= $barang["jenisBarang"]; ?>" readonly>
+          </input>
+        </div>
+        <div class="form-group">
+          <label for="harga">Harga</label>
+          <input type="number" name="hargaBarang" class="form-control" id="harga" value="<?= $barang["hargaJual"]; ?>"></input>
+        </div>
+        <div class="form-group">
+          <label for="jumlah">Jumlah</label>
+          <input type="number" name="jumlah" class="form-control" id="jumlah" value="<?= $barang["jumlah"]; ?>"></input>
+        </div>
+        <div class="form-group">
+          <label for="gambar">Gambar</label>
+          <img src="../img/<?= $barang["gambar"]; ?>" alt="" class="rounded-circle">
+          <input type="file" name="gambar" id="gambar" class="form-control">
+        </div>
+        <a href="data-stok-barang.php"><button type="button" class="btn btn-danger float-right">Batal</button></a>
+        <button type="reset" class="btn btn-warning float-right mr-2">Reset</button>
+        <button type="submit" name="simpan" class="btn btn-primary float-right mr-2">Simpan</button>
+      </form>
+    </div>
+  </div>
 
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="../bootstrap-4.4.1/bootstrap-4.4.1/dist/js/jquery-3.4.1.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="../bootstrap-4.4.1/bootstrap-4.4.1/dist/js/bootstrap.js"></script>
-        <script src="../js/script.js"></script>
-    </body>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="../js/script.js"></script>
+</body>
+
 </html>
--- php/functionBarang.php
+++ php/functionBarang.php
@@ -1,115 +1,101 @@
-<?php 
-    // koneksi database 
-    require "koneksi.php";
+<?php
+// koneksi database 
+require "koneksi.php";
 
-    // ambil data gudang
-    function query($query){
-        global $conn;
-        $result=mysqli_query($conn,$query);
-        $rows=[];
-        while($row=mysqli_fetch_assoc($result)){
-            $rows[]=$row;
-        }
-        return $rows;
-    }
+// ambil data gudang
+function query($query)
+{
+  global $conn;
+  $result = mysqli_query($conn, $query);
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
 
-    // tambah data barang
-    function tambah($data){
-        global $conn;
-        $namaBarang=htmlspecialchars($data["namaBarang"]);
-        $jenisBarang=htmlspecialchars($data["jenisBarang"]);
-        $hargaBarang=htmlspecialchars($data["hargaBarang"]);
-        $jumlah=htmlspecialchars($data["jumlah"]);
-        $gambar=upload();
-        if($gambar==false){
-            echo "
-                <script>
-                document.location.href='tambahDataBarang.php';
-                </script>
-            ";       
-            return false;
-        }
+// tambah data barang
+function tambah($data)
+{
+  global $conn;
+  $namaBarang = htmlspecialchars($data["namaBarang"]);
+  $jenisBarang = htmlspecialchars($data["jenisBarang"]);
+  $hargaBarang = htmlspecialchars($data["hargaBarang"]);
+  $jumlah = htmlspecialchars($data["jumlah"]);
+  $gambar = upload();
+  if ($gambar == false) {
+    echo "<script>document.location.href='tambahDataBarang.php';</script>";
+    return false;
+  }
 
-        $tambah="INSERT INTO barang VALUES(
+  $tambah = "INSERT INTO barang VALUES(
             '', '$namaBarang', '$jenisBarang', '$hargaBarang', '$jumlah','$gambar')
         ";
-        mysqli_query($conn,$tambah);
-        return mysqli_affected_rows($conn);
-    }
+  mysqli_query($conn, $tambah);
+  return mysqli_affected_rows($conn);
+}
 
-    // upload gambar
-    function upload(){
-        $namaFile=$_FILES["gambar"]["name"];
-        $tmpName=$_FILES["gambar"]["tmp_name"];
-        $error=$_FILES["gambar"]["error"];
-        $size=$_FILES["gambar"]["size"];
-        
-        // cek apakah gambar sudah dimasukkan
-        if($error===4){
-            echo"
-                <script>
-                alert('Anda belum memasukkan foto');
-                </script>
-            ";
-            return false;
-        }
+// upload gambar
+function upload()
+{
+  $namaFile = $_FILES["gambar"]["name"];
+  $tmpName = $_FILES["gambar"]["tmp_name"];
+  $error = $_FILES["gambar"]["error"];
+  $size = $_FILES["gambar"]["size"];
 
-        // cek apakah yang diupload == gambar
-        $tipeValid=['jpg','jpeg','png'];
-        $tipeFile=explode('.',$namaFile);
-        $tipeFile=strtolower(end($tipeFile));
-        
-        if(in_array($tipeFile,$tipeValid)===false){
-            echo"
-            <script>
-            alert('Yang anda masukkan bukan gambar!');
-            </script>
-        ";
-        return false;
-        }
+  // cek apakah gambar sudah dimasukkan
+  if ($error === 4) {
+    echo "<script>alert('Anda belum memasukkan foto');</script>";
+    return false;
+  }
 
-        // membatasi ukuran upload
-        if($size>1000000){
-            echo"
-            <script>
-            alert('Ukuran gambar maksimal 1MB');
-            </script>
-        ";
-        return false;
-        }
+  // cek apakah yang diupload == gambar
+  $tipeValid = ['jpg', 'jpeg', 'png'];
+  $tipeFile = explode('.', $namaFile);
+  $tipeFile = strtolower(end($tipeFile));
 
-        // membuat nama gambar baru agar berbeda
-        $namaFileBaru=uniqid();
-        $namaFileBaru.=$tipeFile;
-        move_uploaded_file($tmpName,'../img/'.$namaFileBaru);
-        return $namaFileBaru;
-    }
+  if (in_array($tipeFile, $tipeValid) === false) {
+    echo "<script>alert('Yang anda masukkan bukan gambar!');</script>";
+    return false;
+  }
 
-    // hapus data barang
-    function hapus($idBarang){
-        global $conn;
-        mysqli_query($conn,"DELETE FROM barang WHERE idBarang='$idBarang'");
-        return mysqli_affected_rows($conn);
-    }
+  // membatasi ukuran upload
+  if ($size > 1000000) {
+    echo "<script>alert('Ukuran gambar maksimal 1MB');</script>";
+    return false;
+  }
 
-    // edit data barang
-    function edit($data){
-        global $conn;
-        $idBarang=($data["idBarang"]);
-        $namaBarang=htmlspecialchars($data["namaBarang"]);
-        $jenisBarang=htmlspecialchars($data["jenisBarang"]);
-        $hargaBarang=htmlspecialchars($data["hargaBarang"]);
-        $jumlah=htmlspecialchars($data["jumlah"]);
-        $gambarLama=htmlspecialchars($data["gambarLama"]);
-        if($_FILES["gambar"]["error"]==4){
-            $gambar=$gambarLama;
-        }else{
-            $gambar=upload();
-        }
-        $edit="UPDATE barang SET
-            namaBarang='$namaBarang', jenisBarang='$jenisBarang', hargaJual='$hargaBarang', jumlah='$jumlah', gambar='$gambar' WHERE idBarang=$idBarang;
-        ";
-        mysqli_query($conn,$edit);
-        return mysqli_affected_rows($conn);
-    }
-?>
+  // membuat nama gambar baru agar berbeda
+  $namaFileBaru = uniqid();
+  $namaFileBaru .= $tipeFile;
+  move_uploaded_file($tmpName, '../img/' . $namaFileBaru);
+  return $namaFileBaru;
+}
+
+// hapus data barang
+function hapus($idBarang)
+{
+  global $conn;
+  mysqli_query($conn, "DELETE FROM barang WHERE idBarang='$idBarang'");
+  return mysqli_affected_rows($conn);
+}
+
+// edit data barang
+function edit($data)
+{
+  global $conn;
+  $idBarang = ($data["idBarang"]);
+  $namaBarang = htmlspecialchars($data["namaBarang"]);
+  $jenisBarang = htmlspecialchars($data["jenisBarang"]);
+  $hargaBarang = htmlspecialchars($data["hargaBarang"]);
+  $jumlah = htmlspecialchars($data["jumlah"]);
+  $gambarLama = htmlspecialchars($data["gambarLama"]);
+  if ($_FILES["gambar"]["error"] == 4) {
+    $gambar = $gambarLama;
+  } else {
+    $gambar = upload();
+  }
+  $edit = "UPDATE barang SET namaBarang='$namaBarang', jenisBarang='$jenisBarang', hargaJual='$hargaBarang', jumlah='$jumlah',        gambar='$gambar' WHERE idBarang=$idBarang;";
+  mysqli_query($conn, $edit);
+  return mysqli_affected_rows($conn);
+}
